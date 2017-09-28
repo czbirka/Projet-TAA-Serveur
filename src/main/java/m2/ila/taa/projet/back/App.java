3c104015d0043ed8761f09cd74ed22feef2eb3ef
czbirka <christophe.zbirka@wanadoo.fr>
2017-09-28 21:27:25 +0200

    commit initia du projet

---
+++ src/main/java/m2/ila/taa/projet/back/App.java
@@ -0,0 +1,13 @@
+package m2.ila.taa.projet.back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
